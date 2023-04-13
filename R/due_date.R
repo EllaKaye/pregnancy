18e3357c93b097035a3e151a5b7ee1f858a86944
EllaKaye <ella.kaye@gmail.com>
2023-04-13 21:18:13 +0100

    refactor ovulation_date_calculation

--- R/due_date.R
+++ R/due_date.R
@@ -24,8 +24,8 @@ calculate_due_date <- function(start_date,
                                               "transfer_day_5",
                                               "transfer_day_6"),
                                cycle = 28) {
-
-  ovulation_date <- ovulation_date_calculation(start_date, start_type, cycle)
+  ovulation_date <-
+    ovulation_date_calculation(start_date, start_type, cycle)
   due_date <- ovulation_date + lubridate::days(266)
 
   birth_period_start <- due_date - lubridate::days(21)
@@ -45,46 +45,35 @@ calculate_due_date <- function(start_date,
 # ovulation maybe not right term to use for all fertility treatment types,
 # but the broad idea is good enough for unexported function.
 ovulation_date_calculation <- function(start_date,
-                                 start_type = c("LMP",
-                                                "conception",
-                                                "transfer_day_3",
-                                                "transfer_day_5",
-                                                "transfer_day_6"),
-                                 cycle = 28) {
-
+                                       start_type = c("LMP",
+                                                      "conception",
+                                                      "transfer_day_3",
+                                                      "transfer_day_5",
+                                                      "transfer_day_6"),
+                                       cycle = 28) {
   start_type <- rlang::arg_match(start_type)
 
   check_date(start_date)
 
-  # LMP: start_date is start of last menstrual period
-  if (start_type == "LMP") {
+  if (start_type == "LMP")
     check_cycle(cycle)
-    ovulation_date <-
-      start_date + lubridate::days(cycle) - lubridate::days(28) + lubridate::days(14)
-  }
 
+  # LMP: start_date is start of last menstrual period
   # conception: start_date in date of conception
-  if (start_type == "conception") {
-    ovulation_date <- start_date
-  }
+  # transfer_day_3/5/6: start_date is date of transfer
 
-  # transfer_day_3: start_date is date of transfer
-  if (start_type == "transfer_day_3") {
-    ovulation_date <- start_date - lubridate::days(3)
-  }
-
-  # transfer_day_5: start_date is date of transfer
-  if (start_type == "transfer_day_5") {
-    ovulation_date <- start_date - lubridate::days(5)
-  }
-
-  # transfer_day_6: start_date is date of transfer
-  if (start_type == "transfer_day_6") {
-    ovulation_date <- start_date - lubridate::days(6)
-  }
+  ovulation_date <- switch(
+    start_type,
+    LMP = start_date + lubridate::days(cycle) - lubridate::days(14),
+    conception = start_date,
+    transfer_day_3 = start_date - lubridate::days(3),
+    transfer_day_5 = start_date - lubridate::days(5),
+    transfer_day_6 = start_date - lubridate::days(6)
+  )
 
   ovulation_date
 }
+
 
 get_due_date <- function() {
   due_date <- getOption("pregnancy.due_date")
@@ -101,16 +90,20 @@ get_due_date <- function() {
 
 set_due_date <- function(due_date) {
   # check date
-  if (!is.null(due_date)) check_date(due_date)
+  if (!is.null(due_date))
+    check_date(due_date)
 
   options("pregnancy.due_date" = due_date)
 
   # TODO: different message if due_date = NULL
 
   cli::cli_alert_success("Due date set as {format(due_date, '%B %d, %Y')}")
-  cli::cli_inform(c(
-    "i" = "Functions in the pregnancy package will now use this due
+  cli::cli_inform(
+    c(
+      "i" = "Functions in the pregnancy package will now use this due
                         date without you needing to supply a value to the `due_date` argument.",
-    "i" = "The setting only holds for this R session",
-    "i" = "To set the due date across sessions, SAY SOMETHING ABOUT EDITING RPROFILE..."))
+      "i" = "The setting only holds for this R session",
+      "i" = "To set the due date across sessions, SAY SOMETHING ABOUT EDITING RPROFILE..."
+    )
+  )
 }
