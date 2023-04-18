1d17f4939755de32f0ff02f2f42fa4d807a4fdf2
EllaKaye <ella.kaye@gmail.com>
2023-04-18 11:44:55 +0100

    Add medication data

---
+++ data-raw/medications.R
@@ -0,0 +1,25 @@
+## code to prepare `medications` dataset goes here
+
+library(tibble)
+library(lubridate)
+
+medications <- tribble(
+  ~medication, ~format, ~quantity, ~start_date, ~stop_date,
+  "progynova", "pills", 3, ymd("2023-04-21"), ymd("2023-04-30"),
+  "LDA", "pills", 1, ymd("2023-04-21"), ymd("2023-05-07"),
+  "prednisolone", "pills", 2, ymd("2023-04-26"), ymd("2023-05-07"),
+  "progynova", "pills", 6, ymd("2023-05-01"), ymd("2023-07-11"),
+  "cyclogest", "pessary", 2, ymd("2023-05-03"), ymd("2023-07-11"),
+  "lubion", "injection", 1, ymd("2023-05-03"), ymd("2023-07-11"),
+  "clexane", "injection", 1, ymd("2023-05-08"), ymd("2023-09-05"),
+  "prednisolone", "pills", 4, ymd("2023-05-08"), ymd("2023-07-11"),
+  "prednisolone", "pills", 3, ymd("2023-07-12"), ymd("2023-07-14"),
+  "prednisolone", "pills", 2, ymd("2023-07-15"), ymd("2023-07-17"),
+  "prednisolone", "pills", 1, ymd("2023-07-18"), ymd("2023-07-20"),
+  "prednisolone", "pills", 1, ymd("2023-07-22"), ymd("2023-07-22"),
+  "prednisolone", "pills", 1, ymd("2023-07-24"), ymd("2023-07-24"),
+  "prednisolone", "pills", 1, ymd("2023-07-26"), ymd("2023-07-26")
+)
+
+
+usethis::use_data(medications, overwrite = TRUE)
