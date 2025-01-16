c761c114d499043b643ef4fc10777a56dde332cc
EllaKaye <ella.kaye@gmail.com>
2025-01-16 09:31:20 +0000

    date_when function working

--- R/how_far.R
+++ R/how_far.R
@@ -69,11 +69,72 @@ how_far <- function(on_date = Sys.Date(), due_date = NULL, person = NULL) {
 }
 
 # A reworking of how_long_until
+# TODO: incorporate on_date (or think about whether I need that for anything other than testing)
+# TODO: check due date in relation to on_date and give appropriate message if > 42 weeks pregnant
 date_when <- function(weeks, days = 0, on_date = Sys.Date(), due_date = NULL, person = NULL) {
   check_date(on_date)
 
   due_date <- due_date %||% getOption("pregnancy.due_date") %||% date_abort(due_date)
   check_date(due_date)  
+
+  # date calculations
+  start_date <- due_date - 280
+  date_when <- start_date + (weeks * 7) + days
+  total_days <- abs(as.integer(difftime(date_when, Sys.Date(), units = "days"))) # days from today
+
+  # grammar for output message
+  person <- person %||% getOption("pregnancy.person") %||% "You"
+  subject <- get_subject(person) # "I", "You" or person
+  tense <- get_tense(Sys.Date(), date_when) # "present", "past", "future"
+  verb <- to_be(subject, tense)
+  subject <- ifelse(subject == "You", "you", subject)
+
+  weeks_from_now <- floor(total_days / 7)
+  and_days_from_now <- round(total_days %% 7)
+
+  if (tense == "present") {
+    prefix <- "Today"
+  } else {
+    prefix <- cli::format_inline("On {format(date_when, '%B %d, %Y')}")
+  } 
+
+  if (days == 0) {
+    day_str <- ""
+  } else {
+    day_str <- cli::format_inline("and {days} day{?s}")
+  }
+
+  date_str <- cli::format_inline("{prefix}, {subject} {verb} {weeks} weeks {day_str} pregnant.")
+
+  if (tense != "present") {
+    if (tense == "past") {
+      prefix <- "That was"
+      suffix <- "ago"
+    } else if (tense == "future") {
+      prefix <- "That's"
+      suffix <- "away"
+    }
+
+    if (weeks_from_now == 0) {
+      weeks_str <- ""
+    } else {
+      weeks_str <- cli::format_inline("{weeks_from_now} week{?s} and")
+    }
+  
+    duration_str <- cli::format_inline("{prefix} {weeks_str} {and_days_from_now} day{?s} {suffix}.")
+  }
+
+  # print out information
+  cli::cli_inform(c(
+    "i" = date_str
+  ))
+
+  if (tense != "present") {
+    cli::cli_inform(c(
+      "i" = duration_str
+    ))    
+  }
+
 }
 
 # TODO: make this `date_when` and bring in line with my python implementation
