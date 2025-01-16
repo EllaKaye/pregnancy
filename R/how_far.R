e5ed9315462d346e15177e3b67b1a0d0f101fe85
EllaKaye <ella.kaye@gmail.com>
2025-01-16 14:29:24 +0000

    Split out date_when function

--- R/how_far.R
+++ R/how_far.R
@@ -4,6 +4,7 @@
 
 # TODO: document
 # TODO: check due date in relation to on_date/Sys.Date()
+# TODO: separate calculation and message
 how_far <- function(on_date = Sys.Date(), due_date = NULL, person = NULL) {
   check_date(on_date)
 
@@ -68,10 +69,7 @@ how_far <- function(on_date = Sys.Date(), due_date = NULL, person = NULL) {
   invisible(days_along)
 }
 
-# A reworking of how_long_until
-# TODO: incorporate on_date (or think about whether I need that for anything other than testing)
-# TODO: check due date in relation to on_date and give appropriate message if > 42 weeks pregnant
-date_when <- function(weeks, days = 0, on_date = Sys.Date(), due_date = NULL, person = NULL) {
+date_when_calculation <- function(weeks, on_date = Sys.Date(), due_date = NULL) {
   check_date(on_date)
 
   due_date <- due_date %||% getOption("pregnancy.due_date") %||% date_abort(due_date)
@@ -79,9 +77,16 @@ date_when <- function(weeks, days = 0, on_date = Sys.Date(), due_date = NULL, pe
 
   # date calculations
   start_date <- due_date - 280
-  date_when <- start_date + (weeks * 7) + days
-  total_days <- abs(as.integer(difftime(date_when, Sys.Date(), units = "days"))) # days from today
+  date_when <- start_date + (weeks * 7)
+  # on_date should always be "Sys.Date()", except for testing and documenting purposes
+  total_days <- abs(as.integer(difftime(date_when, on_date, units = "days"))) # days from today
+  list(total_days = total_days, date_when = date_when)
+}
 
+# TODO: when testing, take into account that cli is taking care of extraneous white space
+# returned strings aren't necessarily what's printed by cli
+# TODO: maybe put `days = 0` argument back for more precise messages?
+date_when_message <- function(total_days, date_when, weeks, person = NULL) {
   # grammar for output message
   person <- person %||% getOption("pregnancy.person") %||% "You"
   subject <- get_subject(person) # "I", "You" or person
@@ -98,13 +103,7 @@ date_when <- function(weeks, days = 0, on_date = Sys.Date(), due_date = NULL, pe
     prefix <- cli::format_inline("On {format(date_when, '%B %d, %Y')}")
   } 
 
-  if (days == 0) {
-    day_str <- ""
-  } else {
-    day_str <- cli::format_inline("and {days} day{?s}")
-  }
-
-  date_str <- cli::format_inline("{prefix}, {subject} {verb} {weeks} weeks {day_str} pregnant.")
+  date_str <- cli::format_inline("{prefix}, {subject} {verb} {weeks} weeks pregnant.")
 
   if (tense != "present") {
     if (tense == "past") {
@@ -120,26 +119,34 @@ date_when <- function(weeks, days = 0, on_date = Sys.Date(), due_date = NULL, pe
     } else {
       weeks_str <- cli::format_inline("{weeks_from_now} week{?s} and")
     }
-  
     duration_str <- cli::format_inline("{prefix} {weeks_str} {and_days_from_now} day{?s} {suffix}.")
   } else {
     duration_str = NULL
   }
+  
+  invisible(list(date_str = date_str, duration_str = duration_str))
+}
+
+# A reworking of how_long_until
+# TODO: put `on_date` into a testable helper function, but MAYBE not into date_when
+  # Need to check how this is handled in vignette and example building
+# TODO: check due date in relation to on_date and give appropriate message if > 42 weeks pregnant
+# TODO: maybe put `days = 0` argument back for more precise messages?
+date_when <- function(weeks, on_date = Sys.Date(), due_date = NULL, person = NULL) {
+  dd_calc <- date_when_calculation(weeks = weeks, on_date = on_date, due_date = due_date)
+
+  dd_message <- date_when_message(total_days = dd_calc$total_days, date_when = dd_calc$date_when, weeks = weeks, person = person)
 
   # print out information
   cli::cli_inform(c(
-    "i" = date_str
+    "i" = dd_message$date_str
   ))
 
-  if (tense != "present") {
+  if (!is.null(dd_message$duration_str)) {
     cli::cli_inform(c(
-      "i" = duration_str
+      "i" = dd_message$duration_str
     ))    
   }
-
-  # TODO: when testing, take into account that cli is taking care of extraneous white space
-  # returned strings aren't necessarily what's printed
-  invisible(list(date_str = date_str, duration_str = duration_str))
 }
 
 # TODO: make this `date_when` and bring in line with my python implementation
